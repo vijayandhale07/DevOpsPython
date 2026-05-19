eb74e9c3aca8a35c37f82c4b9a04356540de932f
VijayAndhale <vijayandhale07@gmail.com>
2026-05-19 23:00:24 +0530

    prog2

---
+++ print_function.py
@@ -0,0 +1,18 @@
+planet1="Closest of sun"
+
+print("The planet is", planet1)
+
+print(planet1[0])
+print(planet1[1])
+print(planet1[2])
+print(planet1[3])
+print(planet1[4])
+
+
+print("##########################")
+print("The planet is " + planet1)
+print("The planet is {}".format(planet1))
+print(f"The planet is {planet1}")
+print("The planet is %s" % planet1)
+print("The planet is " + str(planet1))
+print("The planet is " + repr(planet1))
