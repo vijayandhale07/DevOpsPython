868bff20f85df56d868541a86097a45b7bbcd15f
VijayAndhale <vijayandhale07@gmail.com>
2026-05-19 19:18:47 +0530

    program-01

--- Python_level_0.1.py
+++ Python_level_0.1.py
@@ -1,4 +1,4 @@
-with open("Python_level_0.1.txt", "r") as file:
-    content = file.read()
-    print(content)
-     
+with open("aws_s3_bucket_create.py", "r") as file:
+    code = file.read()
+    if "boto3" in code:
+        print("The code uses the boto3 library to interact with AWS services.")
---
+++ multivalue_var.py
@@ -0,0 +1,32 @@
+a = b = c = d = 10
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print("Variable a value is: ", a)
+print("Variable b value is: ", b)
+print("Variable c value is: ", c)
+print("Variable d value is: ", d)   
+print("")
+
+print("Variable a is of type: ", type(a))
+print("Variable b is of type: ", type(b))
+print("Variable c is of type: ", type(c))
+print("Variable d is of type: ", type(d)) 
+# Multiple values assignnment to multiple variables
+a, b, c, d = "alpha", "beta", 10.4, 20
+print("Variable a value is: ", a)
+print("Variable b value is: ", b)
+print("Variable c value is: ", c)
+print("Variable d value is: ", d)   
+print("")
+print("Variable a is of type: ", type(a))
+print("Variable b is of type: ", type(b))
+print("Variable c is of type: ", type(c))
+print("Variable d is of type: ", type(d)) 
+print(a)
+print(b)
+print(c)
+print(d)
