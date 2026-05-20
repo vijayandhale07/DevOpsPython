7a8daa0bf40c235f27e75e76f22512bc2b738f93
VijayAndhale <vijayandhale07@gmail.com>
2026-05-20 10:26:08 +0530

    prog2

---
+++ arithmetic_operator.py
@@ -0,0 +1,45 @@
+# Arithmetic Operators 
+x = 5
+y = 10
+
+total = x + y 
+print(total)
+
+total =  x - y
+print(total)
+
+total = x * y # Multiplication
+print(total)
+
+total = y / x # Division
+print(total)
+
+total = y // x # floor division
+print(total)
+
+total = y % x # modulus
+print(total)
+
+total = x ** 2 # exponentiation
+print(total)
+
+# Comparison Operators 
+print(x > y) # Greater than 
+print(x < y) # Less than
+print(x >= y) # Greater than or equal to
+print(x <= y) # Less than or equal to
+print(x == y) # Equal to
+print(x != y) # Not equal to
+print(x is y) # Identity operator
+print(x is not y) # Not identity operator
+print(x in [1, 2, 3, 4, 5]) # Membership operator
+print(x not in [1, 2, 3, 4, 5]) # Not membership operator
+
+a = 30
+b = 60 
+out = (a > b) and (a < b)
+print(out)
+
+out = (a > b) or (a < b)
+print(out)
+
