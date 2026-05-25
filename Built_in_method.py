2def7624e0611ab954d1c144fcfaa5f8d0c19fdb
VijayAndhale <vijayandhale07@gmail.com>
2026-05-25 10:13:08 +0530

    prog-01 update

--- Built_in_method.py
+++ Built_in_method.py
@@ -10,11 +10,43 @@ Message = message.title()
 print(Message)
 
 # dir function is used to find the built in methods of a data type
-print(dir(message))
-print(dir(str))
 
 print(message.count("e"))
 print(message.count("e", 0, 20))
 print(message.find("career"))
 print(message.find("career", 0, 30))
 print(message.index("career"))
+
+"""
+print(dir(message))
+print("#############")
+print(dir(str))
+print("#############")
+print(dir(int))
+print("#############")
+print(dir(list))
+print("#############")
+print(dir([]))
+print("#############")
+print(dir(()))
+print("#############")
+print(dir({}))
+"""
+
+print(message.split())
+print(message.split(" "))
+print(message.split(" ", 3))
+print(message.isupper())
+print(message.islower())
+print(message.isalpha())
+print(message.isdigit())
+print(message.isalnum())
+print(message.startswith("DevOps"))
+print(message.endswith("professionals"))
+print(message.replace("DevOps", "Cloud"))
+print(message.replace(" ", "_"))
+
+message1 = "    DevOps is the best career    "
+print(message1)
+print(message1.strip()) # removes leading and trailing spaces
+print(message1.strip("D")) # removes leading and trailing D
